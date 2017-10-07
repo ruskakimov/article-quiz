f2f555e8790f10bf0d85d3646980b18cf2c87542
Rustem Kakimov <ruskakimov@gmail.com>
2017-10-07 13:14:38 +0800

    highlight first, get answer from keypress (z,x,c), show answer correctness

--- content_script.js
+++ content_script.js
@@ -20,8 +20,8 @@ if (!window._my_extension_memory) {
             innerHTML: e.target.innerHTML
         }
         document.removeEventListener('mouseover', window._my_extension_memory.listeners.mouseover, false)
-        document.removeEventListener('click', window._my_extension_memory.listeners.click, false)
         window._my_extension_memory.state.mouseoverListener = false
+        document.removeEventListener('click', window._my_extension_memory.listeners.click, false)
         window._my_extension_memory.state.clickListener = false
         document.querySelector('.my-extension-message').style.display = 'none'
 
@@ -29,20 +29,51 @@ if (!window._my_extension_memory) {
         var aReg = /(A | a )/g
         var anReg = /(An | an )/g
         var modif = e.target.innerHTML
-        modif = modif.replace(theReg, '<span class="my-extension-yellow" data-truth="the" data-original="$&"> _ </span>')
-        modif = modif.replace(aReg, '<span class="my-extension-yellow" data-truth="a" data-original="$&"> _ </span>')
-        modif = modif.replace(anReg, '<span class="my-extension-yellow" data-truth="an" data-original="$&"> _ </span>')
+        modif = modif.replace(theReg, '<span class="my-extension-field" data-truth="the" data-original="$&"> _ </span>')
+        modif = modif.replace(aReg, '<span class="my-extension-field" data-truth="a" data-original="$&"> _ </span>')
+        modif = modif.replace(anReg, '<span class="my-extension-field" data-truth="an" data-original="$&"> _ </span>')
         e.target.innerHTML = modif
+
+        window._my_extension_memory.current_el = document.querySelector('.my-extension-field')
+        window._my_extension_memory.current_el.classList.add('my-extension-field-focused')
     }
 
     var keypressHandler = function(e) {
+        var end = function() {
+            console.log('the end')
+            document.removeEventListener('keypress', window._my_extension_memory.listeners.mouseover, false)
+            window._my_extension_memory.state.keypressListener = false
+        }
+
+        var check = function(answer) {
+            var el = window._my_extension_memory.current_el
+            if (!el) {
+                end()
+                return
+            }
+            el.classList.remove('my-extension-field')
+            el.classList.remove('my-extension-field-focused')
+            el.innerText = el.dataset.original
+            if (el.dataset.truth === answer) {
+                el.classList.add('my-extension-true')
+            }
+            else {
+                el.classList.add('my-extension-false')
+            }
+            var next = document.querySelector('.my-extension-field')
+            if (next) next.classList.add('my-extension-field-focused')
+            else end()
+            window._my_extension_memory.current_el = next
+        }
         switch (e.code) {
             case 'KeyZ':
-                var el = document.querySelector('.my-extension-yellow')
-                el.classList.remove('my-extension-yellow')
-                el.innerText = el.dataset.original
+                check('the')
                 break;
             case 'KeyX':
+                check('a')
+                break;
+            case 'KeyC':
+                check('an')
                 break;
         }
     }
@@ -60,14 +91,11 @@ promiseDOMready()
         if (!window._my_extension_memory) {
             // first call
             console.log('first call')
-            document.addEventListener('mouseover', applyOutlineToCurrentTarget, false)
-            document.addEventListener('click', clickHandler, false)
-            document.addEventListener('keypress', keypressHandler, true)
             window._my_extension_memory = {}
             window._my_extension_memory.state = {
-                mouseoverListener: true,
-                clickListener: true,
-                keypressListener: true
+                mouseoverListener: false,
+                clickListener: false,
+                keypressListener: false
             }
             window._my_extension_memory.listeners = {
                 mouseover: applyOutlineToCurrentTarget,
@@ -86,20 +114,21 @@ promiseDOMready()
                 window._my_extension_memory.state.selection.DOMelement.innerHTML = window._my_extension_memory.state.selection.innerHTML
                 window._my_extension_memory.state.selection = null
             }
-            if (!window._my_extension_memory.state.mouseoverListener) {
-                console.log('attached mouseover listener')
-                document.addEventListener('mouseover', window._my_extension_memory.listeners.mouseover, false)
-                window._my_extension_memory.state.mouseoverListener = true
-            }
-            if (!window._my_extension_memory.state.clickListener) {
-                console.log('attached click listener')
-                document.addEventListener('click', window._my_extension_memory.listeners.click, false)
-                window._my_extension_memory.state.clickListener = true
-            }
-            if (!window._my_extension_memory.state.keypressListener) {
-                console.log('attached keypress listener')
-                document.addEventListener('keypress', window._my_extension_memory.listeners.keypress, true)
-                window._my_extension_memory.state.keypressListener = true
-            }
+        }
+
+        if (!window._my_extension_memory.state.mouseoverListener) {
+            console.log('attached mouseover listener')
+            document.addEventListener('mouseover', window._my_extension_memory.listeners.mouseover, false)
+            window._my_extension_memory.state.mouseoverListener = true
+        }
+        if (!window._my_extension_memory.state.clickListener) {
+            console.log('attached click listener')
+            document.addEventListener('click', window._my_extension_memory.listeners.click, false)
+            window._my_extension_memory.state.clickListener = true
+        }
+        if (!window._my_extension_memory.state.keypressListener) {
+            console.log('attached keypress listener')
+            document.addEventListener('keypress', window._my_extension_memory.listeners.keypress, false)
+            window._my_extension_memory.state.keypressListener = true
         }
     })
