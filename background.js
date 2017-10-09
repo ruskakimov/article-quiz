778b5b685e0358709e98cc15467d297708b31eb5
Rustem Kakimov <ruskakimov@gmail.com>
2017-10-09 18:57:59 +0800

    start rebuilding app architecture, no more :spaghetti: code

--- background.js
+++ background.js
@@ -1,4 +1,30 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.insertCSS(null, {file: "style.css"});
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
-});
+var bgAPP = function() {
+  var extensionActive = {}
+
+  return {
+    clickHandler: function(tab) {
+      console.log(extensionActive)
+      if (extensionActive[tab.id]) {
+        console.log('exit')
+        chrome.tabs.sendMessage(tab.id, {exit: true})
+      }
+      else {
+        console.log('start')
+        if (extensionActive[tab.id] === 'undefined') {
+          extensionActive[tab.id] = false
+        }
+        chrome.tabs.insertCSS(tab.id, {file: "style.css"})
+        chrome.tabs.executeScript(tab.id, {file: "content_script.js"})
+      }
+      extensionActive[tab.id] = !extensionActive[tab.id]
+    },
+    reloadHandler: function(tabId) {
+      console.log('reload', tabId)
+      extensionActive[tabId] = false
+    }
+  }
+}()
+
+
+chrome.browserAction.onClicked.addListener(bgAPP.clickHandler)
+chrome.tabs.onUpdated.addListener(bgAPP.reloadHandler)
